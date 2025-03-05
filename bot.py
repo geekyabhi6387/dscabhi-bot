8ce59d7b32341d61b37121c390d60aeefac241f1
Abhyuday <geekyabhi6387@gmail.com>
2025-03-05 17:32:20 +0530

    Functionality for slash commands

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 import discord 
+from discord.ext import commands
+from discord import app_commands
 from os import environ
 from dotenv import load_dotenv
 from google import genai
@@ -9,13 +11,21 @@ api = environ["api_key"]
 
 aiclient = genai.Client(api_key=api)
 chat = aiclient.chats.create(model="gemini-2.0-flash")
-sys_instruct="You are a discord bot which is meant to reply to users chatting in the respective server channels. However you are only allowed to reply in 2000 characters or less because that's the discord chat limit"
+sys_instruct="You are a discord bot which is meant to reply to users chatting in the respective server channels. However you are only allowed to reply in 1800 characters or less because that's the discord chat limit, so always check your response to be in limit"
 
 token = environ["TOKEN"]
 
-class Client(discord.Client):
+class Client(commands.Bot):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
+
+        try:
+            guild = discord.Object(id=579995729620369409)
+            synced = await self.tree.sync(guild=guild)
+            print(f'Synced {len(synced)} commands to guild {guild.id}')
+
+        except Exception as e:
+            print(f'Error syncing commands: {e}')
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
@@ -37,6 +47,8 @@ class Client(discord.Client):
 
 intents  = discord.Intents.default()
 intents.message_content = True
+client = Client(command_prefix="!", intents=intents)
 
-client = Client(intents=intents)
+GUILD_ID = discord.Object(id=579995729620369409)
+
 client.run(token)
