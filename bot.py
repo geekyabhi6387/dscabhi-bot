9dc7594a204d27bf229ef5cd6efd1d47ac328858
Abhyuday <geekyabhi6387@gmail.com>
2025-03-05 15:10:03 +0530

    Can respond to messages through Gemini API

--- bot.py
+++ bot.py
@@ -2,13 +2,14 @@ import discord
 from os import environ
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 load_dotenv()
 
 api = environ["api_key"]
 
 aiclient = genai.Client(api_key=api)
 chat = aiclient.chats.create(model="gemini-2.0-flash")
-
+sys_instruct="You are a discord bot which is meant to reply to users chatting in the respective server channels."
 
 token = environ["TOKEN"]
 
@@ -18,6 +19,20 @@ class Client(discord.Client):
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
+
+        if message.author == client.user:
+            return
+
+        response = aiclient.models.generate_content(
+        model="gemini-2.0-flash",
+        config=types.GenerateContentConfig(
+        system_instruction=sys_instruct),
+        contents=[message.content]
+            )
+
+        gemini_response = response.text
+
+        await message.channel.send(gemini_response)
 
 
 intents  = discord.Intents.default()
